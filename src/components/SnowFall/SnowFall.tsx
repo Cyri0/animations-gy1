17da395de9e52af6e5417583fe9209405879572a
Nemes Tamás - CAD1 <nemestamas94@gmail.com>
2026-04-21 09:04:31 +0200

    Increase snowflake count to 100 and optimize snowflake rendering logic

--- src/components/SnowFall/SnowFall.tsx
+++ src/components/SnowFall/SnowFall.tsx
@@ -9,56 +9,67 @@ type SnowFlakeType = {
 }
 
 const SnowFall = () => {
-  const snowflakeNumber = 100
-  const [snowflakes, setSnowflakes] = useState<SnowFlakeType[]>([])
-  
-  useEffect(()=>{
-    for(let i = 0; i < snowflakeNumber; i++){
-        setSnowflakes(prev => [...prev, {
-            symbol: "❄️",
-            size: Math.floor(Math.random()*2) + 1,
-            speed: Math.floor(Math.random()*5) + 5,
-            xPos: Math.floor(Math.random()*window.innerWidth) + 1,
-            yPos: Math.floor(Math.random()*window.innerHeight) + 1
-        }])
-    }
+    const snowflakeNumber = 30
+    const [snowflakes, setSnowflakes] = useState<SnowFlakeType[]>([])
 
-    const intervalId = setInterval(()=>{
-        setSnowflakes(prev => prev.map(snowflake => {
-            let newY =  window.innerHeight < snowflake.yPos ?
-                        0 :
-                        snowflake.yPos + snowflake.speed;
-            return {
-                ...snowflake,
-                yPos: newY,
-            }
-        }))
-    }, 30)
+    useEffect(() => {
+        for (let i = 0; i < snowflakeNumber; i++) {
+            setSnowflakes(prev => [...prev, {
+                symbol: "❄️",
+                size: Math.floor(Math.random() * 2) + 1,
+                speed: Math.floor(Math.random() * 5) + 5,
+                xPos: Math.floor(Math.random() * window.innerWidth) + 1,
+                yPos: Math.floor(Math.random() * window.innerHeight) + 1
+            }])
+        }
 
-    return () => clearInterval(intervalId)
-  },[])
+        window.addEventListener("resize", () => {
+            console.log("Event listener added");
 
-  return (
-    <div style={{
-        width: "100vw",
-        height: "100vh",
-        position: "fixed",
-        top: "0px",
-        left: "0px",
-        overflow: "hidden",
-        zIndex: -1
-    }}>
-        {snowflakes.map(snowflake => 
+            setSnowflakes(prev => prev.map(snowflake => {
+                return {
+                    ...snowflake,
+                    xPos: Math.floor(Math.random() * window.innerWidth) + 1,
+                }
+            }))
+        })
+
+        const intervalId = setInterval(() => {
+            setSnowflakes(prev => prev.map(snowflake => {
+                let newY = window.innerHeight < snowflake.yPos ?
+                    0 :
+                    snowflake.yPos + snowflake.speed;
+                return {
+                    ...snowflake,
+                    yPos: newY,
+                }
+            }))
+        }, 30)
+
+        return () => clearInterval(intervalId)
+    }, [])
+
+    return (
         <div style={{
-            fontSize: `${snowflake.size}rem`,
-            position: 'absolute',
-            left: `${snowflake.xPos}px`,
-            top: `${snowflake.yPos}px`,
+            width: "100vw",
+            height: "100vh",
+            position: "fixed",
+            top: "0px",
+            left: "0px",
+            overflow: "hidden",
+            zIndex: -1
         }}>
-            {snowflake.symbol}
-        </div>)}
-    </div>
-  )
+            {snowflakes.map(snowflake =>
+                <div style={{
+                    fontSize: `${snowflake.size}rem`,
+                    position: 'absolute',
+                    left: `${snowflake.xPos}px`,
+                    top: `${snowflake.yPos}px`,
+                }}>
+                    {snowflake.symbol}
+                </div>)}
+        </div>
+    )
 }
 
 export default SnowFall
