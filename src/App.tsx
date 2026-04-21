721fac5212722f853c7a7606274852571af0022b
Nemes Tamás - CAD1 <nemestamas94@gmail.com>
2026-04-21 13:45:33 +0200

    Add Hammer component with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import AnimatedBorder from "./components/AnimatedBorder/AnimatedBorder"
 import AnimatedTitle from "./components/AnimatedTitle/AnimatedTitle"
 import EmojiWrapper from "./components/Emoji/EmojiWrapper"
+import Hammer from "./components/Hammer/Hammer"
 import NewtonsCradle from "./components/NewtonsCradle/NewtonsCradle"
 import SnowFall from "./components/SnowFall/SnowFall"
 
 const App = () => {
   return (
     <div>
+
+      <Hammer/>
       {/* <EmojiWrapper/>
       <AnimatedBorder/>
       {/* <SnowFall/> */}
-      <AnimatedTitle titles={[
+      {/* <AnimatedTitle titles={[
         {name:"Hello",bgColor:"orange"},
         {name:"Világ",bgColor:"green"},
         {name:"Hihihi",bgColor:"darkred"},
-      ]} />
+      ]} /> */}
       {/* <NewtonsCradle/> */}
     </div>
   )
